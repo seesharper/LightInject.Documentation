ffe10ee965aadd4fb2748a7b3d57ba11dbf0ae24
Bernhard Richter <bernhardrichter@Bernhards-MacBook-Pro.local>
2016-01-14 12:00:51 +0100

    more prebuild stuff

--- prebuild.py
+++ prebuild.py
@@ -5,6 +5,8 @@ def download(url, file):
     outfile = open(file, "wb")
     subprocess.call(["curl", url], stdout=outfile)
 
-
+download('https://raw.githubusercontent.com/seesharper/LightInject/master/readme.md', 'docs/index.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.xUnit/master/readme.md', 'docs/xunit.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.AutoFactory/master/readme.md', 'docs/autofactory.md')
+
+
