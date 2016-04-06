33820be9e78ad70fa9be0c0b990e586ad217635c
Bernhard Richter <bernhard.richter@gmail.com>
2016-04-06 21:00:08 +0200

    Added web api

--- prebuild.py
+++ prebuild.py
@@ -10,6 +10,7 @@ download('https://raw.githubusercontent.com/seesharper/LightInject.xUnit/master/
 download('https://raw.githubusercontent.com/seesharper/LightInject.AutoFactory/master/readme.md', 'docs/autofactory.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Annotation/master/readme.md', 'docs/annotation.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Interception/master/readme.md', 'docs/interception.md')
+download('https://raw.githubusercontent.com/seesharper/LightInject.WebApi/master/readme.md', 'docs/webapi.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Web/master/readme.md', 'docs/web.md')
 download('https://raw.githubusercontent.com/seesharper/LightMock/master/readme.md', 'docs/lightmock.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Microsoft.DependencyInjection/master/readme.md', 'docs/microsoft.dependencyinjection.md')
