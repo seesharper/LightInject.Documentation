ce1200d05923045311a8ff7782f4ca2757b42c4d
Bernhard Richter <bernhard.richter@gmail.com>
2016-08-26 15:21:42 +0200

    Added mvc and missing licence

--- prebuild.py
+++ prebuild.py
@@ -7,7 +7,7 @@ def download(url, file):
 
 download('https://raw.githubusercontent.com/seesharper/LightInject/master/readme.md', 'docs/index.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject/master/designpatterns.md', 'docs/designpatterns.md')
-download('https://raw.githubusercontent.com/seesharper/LightInject/master/licence.md', 'docs/licence.md')
+download('https://raw.githubusercontent.com/seesharper/LightInject.Documentation/master/licence.md', 'docs/licence.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.xUnit/master/readme.md', 'docs/xunit.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.AutoFactory/master/readme.md', 'docs/autofactory.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Annotation/master/readme.md', 'docs/annotation.md')
@@ -16,6 +16,7 @@ download('https://raw.githubusercontent.com/seesharper/LightInject.WebApi/master
 download('https://raw.githubusercontent.com/seesharper/LightInject.Nancy/master/readme.md', 'docs/nancy.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Web/master/readme.md', 'docs/web.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Wcf/master/readme.md', 'docs/wcf.md')
+download('https://raw.githubusercontent.com/seesharper/LightInject.Mvc/master/readme.md', 'docs/mvc.md')
 download('https://raw.githubusercontent.com/seesharper/LightMock/master/readme.md', 'docs/lightmock.md')
 download('https://raw.githubusercontent.com/seesharper/LightInject.Microsoft.DependencyInjection/master/readme.md', 'docs/microsoft.dependencyinjection.md')
 download('https://raw.githubusercontent.com/seesharper/Blog.WebApiRequestLogging/master/readme.md', 'docs/webapirequestlogging.md')
